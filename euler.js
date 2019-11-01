ce28472bccd0445a691829354f7eb4be90ce9075
Curtis Yungen <curtisyungen@gmail.com>
2019-10-31 17:53:40 -0700

    solve problem 2

--- euler.js
+++ euler.js
@@ -1,3 +1,7 @@
+// PROJECT EULER
+// projecteuler.net
+// =====================================================================
+
 // Find the sum of all the multiples of 3 or 5 below 1000.
 function problem1() {
   let sum = 0;
@@ -10,4 +14,25 @@ function problem1() {
   console.log(sum);
 }
 
-problem1();
+// Find the sum of the even-valued terms in a Fibonacci sequence whose values do not exceed four million.
+function problem2() {
+  let sum = 2;
+
+  let fibo = [1, 2, 3];
+  let value = 3;
+  let count = 0;
+
+  while (value < 4000000) {
+    value = fibo[count + 1] + fibo[count + 2];
+    fibo.push(value);
+    count += 1;
+
+    if (value % 2 === 0) {
+      sum += value;
+    }
+  }
+
+  console.log(sum);
+}
+
+problem2();
