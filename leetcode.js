d75e31be14869b0e69664c1c637e73501ddce40e
Curtis Yungen <curtisyungen@gmail.com>
2019-11-01 20:24:05 -0700

    solve problem 7

--- leetcode.js
+++ leetcode.js
@@ -43,3 +43,39 @@ function convert(s, numRows) {
 
   console.log(finalStr);
 }
+
+// Problem 7: Palindrome Number
+// https://leetcode.com/problems/palindrome-number/
+
+var isPalindrome = function(x) {
+  if (x < 0) {
+    return false;
+  }
+
+  if (x < 10) {
+    return true;
+  }
+
+  if (x % 11 === 0 && x < 100) {
+    return true;
+  }
+
+  let newNum = new Array();
+  while (x > 0) {
+    newNum.push(x % 10);
+    x = Math.floor(x / 10);
+  }
+
+  let newArr = new Array();
+  for (var i = newNum.length - 1; i >= 0; i--) {
+    newArr.push(newNum[i]);
+  }
+
+  for (var j in newNum) {
+    if (newNum[j] !== newArr[j]) {
+      return false;
+    }
+  }
+
+  return true;
+};
