4f99ca41d8489edb24e373562296266d37181778
Curtis Yungen <curtisyungen@gmail.com>
2019-11-29 12:53:00 -0800

    start Climbing the Leaderboard

--- hackerrank.js
+++ hackerrank.js
@@ -23,3 +23,27 @@ function pickingNumbers(a) {
 
   return maxLength;
 }
+
+// Problem: Climbing the Leaderboard
+// https://www.hackerrank.com/challenges/climbing-the-leaderboard/problem?h_r=next-challenge&h_v=zen
+
+function climbLeaderboard(scores, alice) {
+  let ranks = [];
+  for (var a in alice) {
+    if (alice[a] > Math.max(...scores)) {
+      ranks.push(1);
+    } else if (alice[a] < Math.min(...scores)) {
+      ranks.push(scores.length);
+    } else {
+      for (var s = 1; s <= scores.length; s++) {
+        if (scores[s] <= alice[a] && scores[s - 1] > alice[a]) {
+          ranks.push(s);
+        }
+      }
+    }
+  }
+
+  return ranks;
+}
+
+console.log(climbLeaderboard([100, 90, 90, 80, 75, 60], [50, 65, 77, 90, 102]));
