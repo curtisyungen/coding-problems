bf284459e351b1195fb82d0e678c95f861fcd6e0
Curtis Yungen <curtisyungen@gmail.com>
2019-12-01 19:43:05 -0800

    solve Taum B-Day

--- hackerrank.js
+++ hackerrank.js
@@ -476,3 +476,15 @@ function acmTeam(topic) {
 
 // ============================================================================
 // Problem:
+// Easy
+
+const BigNumber = require("bignumber.js");
+
+function taumBday(b, w, bc, wc, z) {
+  [b, w, bc, wc, z] = [...arguments].map(x => new BigNumber(x));
+  let tbc = BigNumber.min(b.times(bc), b.times(wc).plus(b.times(z)));
+  let twc = BigNumber.min(w.times(wc), w.times(bc).plus(w.times(z)));
+  return tbc.plus(twc).toFixed();
+}
+
+// ============================================================================
