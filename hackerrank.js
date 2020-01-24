973de2042fc9d023d1eb808bd5b582bf16cf72c9
Curtis Yungen <curtisyungen@gmail.com>
2020-01-24 15:59:58 -0800

    Solve Merge Two Lists

--- hackerrank.js
+++ hackerrank.js
@@ -1846,3 +1846,29 @@ function CompareLists(llist1, llist2) {
 }
 
 // ============================================================================
+// Problem: Merge Two Sorted Linked Lists
+// Easy
+
+function mergeLists(head1, head2) {
+  if (!head1 && !head2) {
+    return null;
+  } else if (!head1) {
+    return head2;
+  } else if (!head2) {
+    return head1;
+  }
+
+  let result;
+
+  if (head1.data <= head2.data) {
+    result = head1;
+    result.next = mergeLists(head1.next, head2);
+  } else {
+    result = head2;
+    result.next = mergeLists(head1, head2.next);
+  }
+
+  return result;
+}
+
+// ============================================================================
