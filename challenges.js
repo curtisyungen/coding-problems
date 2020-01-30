a691db88f4fd103d844d9e2a914b487684f62e7a
Curtis Yungen <39065500+curtisyungen@users.noreply.github.com>
2020-01-30 09:57:36 -0800

    update solution3

--- challenges.js
+++ challenges.js
@@ -57,15 +57,15 @@ function solution2(s) {
 // Problem: find earliest substring in alphabet that can be obtained by removing one character
 
 function solution3(s) {
-  for (var i = 0; i < s.length; i++) {
-    let currVal = s.charCodeAt(i);
-    let prevVal = i - 1 >= 0 ? s.charCodeAt(i - 1) : -1;
-    let nextVal = i + 1 <= s.length ? s.charCodeAt(i + 1) : -1;
-    
-    if (currVal > prevVal && currVal > nextVal) {
-      return s.substring(0, i).concat(s.substring(i + 1, s.length));
+    for (var i = 0; i < s.length; i++) {
+        let currVal = s.charCodeAt(i);
+        let prevVal = i - 1 >= 0 ? s.charCodeAt(i - 1) : -1;
+        let nextVal = i + 1 < s.length ? s.charCodeAt(i + 1) : -1;
+
+        if (currVal > prevVal && currVal >= nextVal) {
+            return s.substring(0, i).concat(s.substring(i + 1, s.length));
+        }
     }
-  }
 }
 
 // console.log(solution3("acb"));
